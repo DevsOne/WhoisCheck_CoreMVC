642cd0e109990978bb46004e1bcc540f918fd8cc
Eldar <Eldar@ELDAR-LAPTOP>
2016-09-23 07:24:19 +0300

    Add project files.

---
+++ src/WhoisCheck_CoreMVC/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WhoisCheck_CoreMVC.Models;
+
+namespace WhoisCheck_CoreMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult TldServerList()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CheckDomain(DomainViewModels model)
+        {
+            string whoisText = null;
+            var tldServer = Whois.GetWhoisServerName(model.DomainName);
+
+            if (tldServer != null) { whoisText = await Whois.GetWhoisInformation(tldServer, model.DomainName); }
+            else { whoisText = await Whois.GetWhoisInformation("whois.verisign-grs.com", model.DomainName + ".com"); }
+
+            whoisText = whoisText.Replace("\r\n", "<br/>");
+            model.DomainDetails = whoisText;
+            return View("Index", model);
+        }
+    }
+}
---
+++ src/WhoisCheck_CoreMVC/Models/ViewModels.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WhoisCheck_CoreMVC.Models
+{
+    public class TLDViewModel
+    {
+        public string TldName { get; set; }
+        public string ASCIIcode { get; set; }
+        public string WhoisServer { get; set; }
+    }
+    public class DomainViewModels
+    {
+        [Display(Name = "Domain Name")]
+        public string DomainName { get; set; }
+        public string DomainDetails { get; set; }
+    }
+}
